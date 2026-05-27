aab8c41b291a7d0f199175d1f8594d45c3b84072
Wei Zhang <kweizh@tabbyml.com>
2026-05-27 18:48:35 +0800

    fix(jobs,tasks): stage b review 1

--- jobs/2026-05-26__11-38-52/concurrency_per_key_py__tBpNUPU/artifacts/user/myproject/test_run.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-import runner
-async def main():
-    ref = await runner.process_payment.aio_run({"user_id": "A"}, wait_for_result=False)
-    print(dir(ref))
-asyncio.run(main())
--- jobs/2026-05-26__11-38-52/cron_schedule_py__BAxNV8H/artifacts/user/myproject/test_task.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-from hatchet_sdk import Hatchet
-hatchet = Hatchet()
-@hatchet.task(name="heartbeat")
-def heartbeat(context):
-    print("Heartbeat task executed")
-print("Task defined")
